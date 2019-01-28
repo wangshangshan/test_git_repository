ac5d4427fb9757c1d34dce6a5520370f83e6ed78
wwy <wwybiji@163.com>
2019-01-28 14:18:54 +0800

    my second commit

---
+++ 2_dir/ttt.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ttt.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2019年01月28日 14时16分01秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	
+
+	printf("this second dir repository\n");
+
+	return 0;
+}
